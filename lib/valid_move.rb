45a542b0abae5ecc48399e10378e28816d91b250
Elliott Vargo <vargoelliott@gmail.com>
2020-12-09 05:24:33 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -10,7 +10,7 @@ def valid_move?(board, index)
   elsif position_taken?(board, index) == true
     return false
 
-  elsif index == between?(1,9)
+  elsif index == between?(-1,9)
     true
 
   else
