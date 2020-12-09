4bf3b5c28fc51b69a4dec29faf0753f984bb4a95
Elliott Vargo <vargoelliott@gmail.com>
2020-12-09 03:19:28 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -6,13 +6,16 @@ end
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 def valid_move?(board, index)
-  if index == between?(0,8)
+
+  if position_taken?(board, index) == false
+  return true
+
+elsif index == between?(0,8)
   return false
+
 elsif position_taken?(board, index) == true
   return false
 
-elsif position_taken?(board, index) == false
-  return true
 
 end
 end
